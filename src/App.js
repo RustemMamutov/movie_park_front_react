03aee6eba16202a9c8aee253b543d2c0616c0c22
Rustem Mamutov <mamutovrm1@gmail.com>
2021-02-09 01:46:52 +0300

    added payment step.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-credit-cards/es/styles-compiled.css';
 import css from './App.module.css';
+import constants from "./scripts/constants";
 
 import MyHeader from "./component/Header/MyHeader"
 import MyFooter from "./component/Footer/MyFooter";
@@ -9,8 +11,9 @@ import MainContent from "./component/MainContent/MainContent";
 import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 import SeancesForAllMovieParks from "./component/Seances/SeancesForAllMovieParks/SeancesForAllMovieParks";
 import {SeanceSchema} from "./component/Seances/SeanceSchema/SeanceSchema";
-import constants from "./scripts/constants";
 import GeneralUtils from "./scripts/GeneralUtils";
+import Payment from "./component/Seances/Payment/Payment";
+import PaymentSuccess from "./component/Seances/Payment/PaymentSuccess";
 
 function App() {
     return (
@@ -20,7 +23,9 @@ function App() {
                 <Switch>
                     <Route path='/movies/active_date=:activeDate' component={MainContent}/>
                     <Route path='/seances/active_date=:activeDate/movie_id=:movieId' component={SeancesForAllMovieParks}/>
-                    <Route path='/seance/seance_id=:seanceId/hall_id=:hallId' component={SeanceSchema}/>
+                    <Route path='/seance/seance_id=:seanceId' component={SeanceSchema}/>
+                    <Route path='/seance/payment/success' component={PaymentSuccess}/>
+                    <Route path='/seance/payment' component={Payment}/>
                     <Redirect from="/" to={`/movies/active_date=${GeneralUtils.todayDateStr()}`}/>
                 </Switch>
                 <MyFooter/>
